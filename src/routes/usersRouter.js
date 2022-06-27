c1ee9b115da307fda815cd0da71d80c2f07951c1
Rafael <rafaelratier@hotmail.com>
2022-06-27 17:50:28 -0300

    feat(routes): adds route to get all users

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { createUser } = require('../controllers/usersController');
+const { createUser, getAllUsers } = require('../controllers/usersController');
 const createUserValidation = require('../middlewares/createUserValidation');
+const authValidation = require('../middlewares/authValidationMIddleware');
 
 const usersRouter = express.Router();
 
 usersRouter.post('/', createUserValidation, createUser);
+
+usersRouter.use(authValidation);
+usersRouter.get('/', getAllUsers);
 
 module.exports = usersRouter;
