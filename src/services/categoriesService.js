0622bbfef10121964c23665f22fe39209f024802
Rafael <rafaelratier@hotmail.com>
2022-06-27 23:08:02 -0300

    fix(categoriesservice): fix on return from query

--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -3,9 +3,12 @@ const { Category } = require('../database/models');
 const createCategory = async (categoryName) => {
   const newCategory = await Category.findOrCreate({
     where: { name: categoryName },
+    defaults: {
+      name: categoryName,
+    },
   });
 
-  return newCategory;
+  return newCategory[0].dataValues;
 };
 
 module.exports = {
